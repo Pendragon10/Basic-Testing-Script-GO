8d28e9d9e12d87e398a5da81e2af5432d5a5710c
Pendragon10 <62668656+Pendragon10@users.noreply.github.com>
2020-06-03 20:56:05 -0400

    Update faveSport.go

--- faveSport.go
+++ faveSport.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Aren't sports fun??")
-}
-
 //Rating for different sports
-func FavoriteSport(sport string) string {
+func SportRating(sport string) string {
 
 	if sport == "Football" || sport == "football" {
 		return "Football: 100/10"
-		/*} else if sport == "Soccer" || sport == "soccer" {
-			return "Soccer: 9/10"
-		} else if sport == "Pingpong" || sport == "pingpong" {
-			return "Pingpong: 10/10"*/
+	} else if sport == "Soccer" || sport == "soccer" {
+		return "Soccer: 9/10"
+	} else if sport == "Pingpong" || sport == "pingpong" {
+		return "Pingpong: 10/10"
 	} else {
 		return "Invalid Sport or Argument"
 	}
 
+}
+
+func main() {
+	fmt.Println("Aren't sports fun??")
+
+	myfavesport := SportRating("Football")
+
+	fmt.Println("My favorite sport is " + myfavesport)
 }
